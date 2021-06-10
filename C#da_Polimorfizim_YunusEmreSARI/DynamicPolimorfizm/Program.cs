62948936fadd9adf47b6df695c8fc13d9d49c2a1
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-10 19:11:41 +0300

    C#da_Polimorfizim_YunusEmreSARI

---
+++ C#da_Polimorfizim_YunusEmreSARI/DynamicPolimorfizm/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSharpPolimorfizm
+{
+
+    //Dynamic Polimorfizm
+
+    public class Sekil
+    {
+        public virtual void Ciz()
+        {
+            Console.WriteLine("Temel sınıf çiz çalıştı.");
+        }
+    }
+
+    public class Dikdortgen : Sekil
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Dikdörtgen sınıf çiz çalıştı.");
+        }
+    }
+
+    public class Ucgen : Sekil
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Üçgen sınıf çiz çalıştı.");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.Ciz();
+
+            Ucgen ucgen = new Ucgen();
+            ucgen.Ciz();
+
+        }
+    }
+
+
+}
+
+
---
+++ C#da_Polimorfizim_YunusEmreSARI/StaticPolimorfizm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpPolimorfizm
+{
+
+
+    //Static Polimorfizm
+
+    public class OrnekSinif
+    {
+        public int Toplama(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Toplama(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OrnekSinif yeniNesne = new OrnekSinif();
+
+            int ucSayiToplami = yeniNesne.Toplama(45, 34, 67);
+            int ikiSayiToplami = yeniNesne.Toplama(23, 34);
+
+            Console.WriteLine("Üç sayının toplamı: " + ucSayiToplami);
+            Console.WriteLine("İki sayının toplamı: " + ikiSayiToplami);
+
+        }
+    }
+
+
+
+
+}
+
+
