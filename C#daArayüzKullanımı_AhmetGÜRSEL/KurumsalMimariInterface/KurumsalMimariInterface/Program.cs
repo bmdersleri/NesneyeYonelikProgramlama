18ca50d9502ddfcc23b9822fae8d3ae2cfd205fd
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-11 12:35:32 +0300

    C#daArayüzKullanımı_AhmetGÜRSEL

---
+++ C#daArayüzKullanımı_AhmetGÜRSEL/KurumsalMimariInterface/KurumsalMimariInterface/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace KurumsalMimariInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBManager example1 = new DBManager(new MsSQLDB());
+            DBManager example2 = new DBManager(new OracleDB());
+            DBManager example3 = new DBManager(new MongoDB());
+
+            example1.delete();
+
+            example2.update();
+
+            example3.save();
+
+            Console.ReadLine();
+        }
+    }
+    class DBManager
+    {
+        private IRepository _repository;
+        public DBManager(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void delete()
+        {
+            _repository.delete();
+        }
+        public void update()
+        {
+            _repository.update();
+        }
+        public void insert()
+        {
+            _repository.insert();
+        }
+        public void save()
+        {
+            _repository.save();
+        }
+    }
+    interface IRepository
+    {
+        void delete();
+        void insert();
+        void update();
+        void save();
+    }
+    class MsSQLDB : IRepository
+    {
+        public void delete()
+        {
+            Console.WriteLine("MsSQL > delete()");
+        }
+        public void insert()
+        {
+            Console.WriteLine("MsSQL > insert()");
+        }
+        public void update()
+        {
+            Console.WriteLine("MsSQL > update()");
+        }
+        public void save()
+        {
+            Console.WriteLine("MsSQL > save()");
+        }
+
+    }
+    class OracleDB : IRepository
+    {
+        public void delete()
+        {
+            Console.WriteLine("OracleDB > delete()");
+        }
+        public void insert()
+        {
+            Console.WriteLine("OracleDB > insert()");
+        }
+        public void update()
+        {
+            Console.WriteLine("OracleDB > update()");
+        }
+        public void save()
+        {
+            Console.WriteLine("OracleDB > save()");
+        }
+    }
+    class MongoDB : IRepository
+    {
+        public void delete()
+        {
+            Console.WriteLine("MongoDB > delete()");
+        }
+        public void insert()
+        {
+            Console.WriteLine("MongoDB > insert()");
+        }
+        public void update()
+        {
+            Console.WriteLine("MongoDB > update()");
+        }
+        public void save()
+        {
+            Console.WriteLine("MongoDB > save()");
+        }
+    }
+}
---
+++ C#daArayüzKullanımı_AhmetGÜRSEL/README.md
@@ -0,0 +1,17 @@
+# C#'da Arayüz Kavramı
+
+C#'da arayüz(interface) kavramı oldukça önemli bir yere sahiptir. Programlamaya yeni başlamış ya da stajyer gibi tecrübesiz yazılımcılar tarafından sıklıkla başka kavramlarla karıştırılmaktadır. Arayüzler kısaca verilecek olan hizmetleri ve özellikleri tanımlar. Tanımlanan işlevlerin nasıl yapılacağı ise alt sınıfların görevidir. Bu sayede özellikle kurumsal ve sürekli büyüyen mimarilerde kodumuz üzerindeki kontrolümüzü yitirmeden yeni özellikler ve hizmetler ekleyebiliriz.
+
+Sunumumda kullandığım basit ama kullanışlı uygulama ile arayüz kavramını kolayca anlayabilirsiniz. Arayüzlerin sürdürülebilir kodlama için ne kadar önemli bir yapı olduğunu kodlar üzerinde değişiklikler ve eklemeler yaparak kendiniz de deneyimleyebilirsiniz.
+
+#
+
+Youtube Kanalımız: BMDersleri
+
+Bağlantı: https://www.youtube.com/bmdersleri
+
+Kısa Bağlantı: https://bit.ly/32k9MnJ
+
+Github Adresimiz: https://github.com/bmdersleri
+
+Hazırlayan: Ahmet GÜRSEL
