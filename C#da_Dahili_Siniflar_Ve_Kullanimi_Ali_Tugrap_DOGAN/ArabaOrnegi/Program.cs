3d26529c85905aafbdef3760e4c93cc0c892c977
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-15 23:57:22 +0300

    C#da_Dahili_Siniflar_Ve_Kullanimi_Ali_Tugrap_DOGAN

---
+++ C#da_Dahili_Siniflar_Ve_Kullanimi_Ali_Tugrap_DOGAN/ArabaOrnegi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ArabaOrnegi
+{
+    internal class Kapı { }
+    class Program
+    {
+
+        public class Araba
+        {
+            protected Kapı önSag;
+            protected Kapı önSol;
+
+            protected class Motor
+            {
+                public int Beygir;
+
+            }
+            protected class Aku
+            {
+                public int voltaj;
+            }
+            public class Kapı
+            {
+                
+                public int pozisyon;
+            }
+
+            protected Motor motor; protected Aku aku;
+            public Araba()
+            {
+                motor = new Motor();
+                aku = new Aku();
+            }
+        }
+
+
+        public class Ford : Araba
+        {
+            public Ford()
+            {
+                motor.Beygir = 1500;
+                aku.voltaj = 123;
+                önSag.pozisyon = 2;
+                önSol.pozisyon = 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
---
+++ C#da_Dahili_Siniflar_Ve_Kullanimi_Ali_Tugrap_DOGAN/Dahili Sınıf/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Dahili_Sınıf
+{
+    class Program
+    {
+        public class Ekmek
+        {
+        }
+
+        public class Kiraz
+        {
+        }
+
+        public class Market
+        {
+            public class FırınReyonu
+            {
+                public Ekmek EkmekSat()
+                {
+                    Console.WriteLine("4 ekmek satıldı.");
+                    return new Ekmek();
+                }
+            }
+
+            public class MeyveReyonu
+            {
+                public Kiraz KirazSat()
+                {
+                    Console.WriteLine("10 kilo kiraz satıldı.");
+                    return new Kiraz();
+                }
+            }
+        }
+
+        static public void Main()
+        {
+            var satis1 = new Market.FırınReyonu();
+            satis1.EkmekSat();
+
+            var satis2 = new Market.MeyveReyonu();
+            satis2.KirazSat();
+        }
+        
+    }
+}
