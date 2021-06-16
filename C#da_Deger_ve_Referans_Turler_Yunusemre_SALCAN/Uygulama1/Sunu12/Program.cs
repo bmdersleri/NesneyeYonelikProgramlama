c30020a9602ee4c3767c5ab69e5db248dc5534d5
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-16 12:42:44 +0300

    C#da_Deger_ve_Referans_Turler_Yunusemre_SALCAN

---
+++ C#da_Deger_ve_Referans_Turler_Yunusemre_SALCAN/Uygulama1/Sunu12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+
+            sayi1 = sayi2;
+
+            sayi2 = 300;
+
+            Console.WriteLine("Sayi1: "+sayi1);
+
+
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ C#da_Deger_ve_Referans_Turler_Yunusemre_SALCAN/Uygulama2/Sunu13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] dersler1 = new string[] { "Matematik, Turkce,Fenbilimleri" };
+            string[] dersler2 = new string[] { "Beden egitmi, muzik, resim" };
+
+
+            dersler1 = dersler2;
+
+            dersler2[0] = "YabanciDil";
+            Console.WriteLine(dersler1[0]);
+
+        }
+    }
+}
+
+
+
+
+
+
+
