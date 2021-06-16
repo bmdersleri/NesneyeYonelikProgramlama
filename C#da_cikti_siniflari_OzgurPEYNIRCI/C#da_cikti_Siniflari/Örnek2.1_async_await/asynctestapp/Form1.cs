2b8a32d1fe8033145cf195f07bc3b089f5514a50
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-16 22:25:42 +0300

    C#da_cikti_siniflari_OzgurPEYNIRCI

---
+++ C#da_cikti_siniflari_OzgurPEYNIRCI/C#da_cikti_Siniflari/Örnek2.1_async_await/asynctestapp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace asynctestapp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private int Ağırİşlem()
+        {
+            for (long i = 0; i <= 1000000000000; i += 500)
+            {
+
+            }
+            return 0;
+        }
+        private async void btnİşlem_Click(object sender, EventArgs e)
+        { 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var görev = Task.Run(() => Ağırİşlem());
+            int toplam = 3 + 2;
+            var bitmişGörev = await görev;
+            sw.Stop();
+            MessageBox.Show("bitti " + sw.Elapsed);
+        }
+    }
+}
---
+++ C#da_cikti_siniflari_OzgurPEYNIRCI/C#da_cikti_Siniflari/Örnek6.3_WebSitesiİndirmeProgramı(Senkron_vs_Asenkron)/WPFUserInterface/WebsiteDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUserInterface
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
