3742038bd3cbe700e1bdc95a0ac4b2c3303a9335
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-16 04:02:02 +0300

    ErisimBelirleyiciler_TugceGokceoglu

---
+++ Javada_ErisimBelirleyiciler_TugceGokceoglu/publicornek/src/paket2/sınıf3.java
@@ -0,0 +1,10 @@
+package paket2;
+import paket1.sınıf1;
+public class sınıf3{
+    public static void main(String[] args) {
+        sınıf1 sınıf=new sınıf1();
+        sınıf.vize=50;
+        sınıf.finall=60;
+        sınıf.ortalama();
+    }
+}
